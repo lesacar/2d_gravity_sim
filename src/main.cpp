c52144c0d728df19e1b03a0173f9d6fd4ff69e68
lesacar <69954403+lesacar@users.noreply.github.com>
2025-05-31 19:12:56 +0200

    basic frame info

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,20 @@
 #include <math.h>
 
 int main (int argc, char *argv[]) {
+    SetConfigFlags(FLAG_VSYNC_HINT);
     InitWindow(1280, 720, "window");
     bool running = true;
+    // if we should show fps, frametime, other stuff like that
+    bool sim_debug_info = true;
     while (running && !WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawText("test", 20, 20, 20, WHITE);
+        if (sim_debug_info) {
+            // DrawFPS(10, 10);
+            DrawText(TextFormat("%04d FPS", GetFPS()), 10, 10, 20, WHITE);
+            DrawText(TextFormat("FrameTime: %.2f ms", GetFrameTime()*1000.0f), 10, 30, 20, WHITE);
+        }
+        // DrawText("test", 20, 20, 20, WHITE);
         EndDrawing();
     }
     return 0;
